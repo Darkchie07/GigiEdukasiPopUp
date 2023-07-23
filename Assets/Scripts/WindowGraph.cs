c57d2c38db481065b13ecc0a09f4cda2bd5b1f06
Hanif <85260485+hxnmi@users.noreply.github.com>
2023-07-23 22:15:32 +0700

    Add axis separator graph

--- Assets/Scripts/WindowGraph.cs
+++ Assets/Scripts/WindowGraph.cs
@@ -10,6 +10,10 @@ public class WindowGraph : MonoBehaviour
     public WindowGraph Instance;
     [SerializeField] private Sprite circleSprite;
     private RectTransform graphContainer;
+    private RectTransform labelTemplateX;
+    private RectTransform labelTemplateY;
+    private RectTransform dashTemplateX;
+    private RectTransform dashTemplateY;
 
     public List<int> SkorPengetahuan = new List<int>();
     public List<int> SkorSikap = new List<int>();
@@ -19,6 +23,10 @@ public class WindowGraph : MonoBehaviour
     private void Awake()
     {
         graphContainer = transform.Find("graphContainer").GetComponent<RectTransform>();
+        labelTemplateX = graphContainer.Find("labelTemplateX").GetComponent<RectTransform>();
+        labelTemplateY = graphContainer.Find("labelTemplateY").GetComponent<RectTransform>();
+        dashTemplateX = graphContainer.Find("dashTemplateX").GetComponent<RectTransform>();
+        dashTemplateY = graphContainer.Find("dashTemplateY").GetComponent<RectTransform>();
 
         List<int> value = new List<int>() { 8, 8, 8, 6, 8 };
         ShowGraph(value);
@@ -40,8 +48,8 @@ public class WindowGraph : MonoBehaviour
     private void ShowGraph(List<int> valueList)
     {
         float graphHeight = graphContainer.sizeDelta.y;
-        float yMaximum = 50f;
-        float xSize = 100f;
+        float yMaximum = 10f;
+        float xSize = 50f;
 
         GameObject lastCircleGameObject = null;
         for (int i = 0; i < valueList.Count; i++)
@@ -54,6 +62,33 @@ public class WindowGraph : MonoBehaviour
                 CreateDotConnection(lastCircleGameObject.GetComponent<RectTransform>().anchoredPosition, circleGameObject.GetComponent<RectTransform>().anchoredPosition);
             }
             lastCircleGameObject = circleGameObject;
+
+            RectTransform labelX = Instantiate(labelTemplateX);
+            labelX.SetParent(graphContainer, false);
+            labelX.gameObject.SetActive(true);
+            labelX.anchoredPosition = new Vector2(xPosition, -7f);
+            labelX.GetComponent<Text>().text = i.ToString();
+
+            RectTransform dashY = Instantiate(dashTemplateY);
+            dashY.SetParent(graphContainer, false);
+            dashY.gameObject.SetActive(true);
+            dashY.anchoredPosition = new Vector2(xPosition, -5f);
+        }
+
+        int separatorCount = 10;
+        for (int i = 0; i <= separatorCount; i++)
+        {
+            RectTransform labelY = Instantiate(labelTemplateY);
+            labelY.SetParent(graphContainer, false);
+            labelY.gameObject.SetActive(true);
+            float normalizedValue = i * 1f / separatorCount;
+            labelY.anchoredPosition = new Vector2(-10f, normalizedValue * graphHeight);
+            labelY.GetComponent<Text>().text = Mathf.RoundToInt(normalizedValue * yMaximum).ToString();
+
+            RectTransform dashX = Instantiate(dashTemplateX);
+            dashX.SetParent(graphContainer, false);
+            dashX.gameObject.SetActive(true);
+            dashX.anchoredPosition = new Vector2(0, normalizedValue * graphHeight - 5f);
         }
     }
 
